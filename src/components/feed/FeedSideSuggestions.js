2fcf75192744fcef0e61ccae6957ff9a3c613ed1
Morgan Segura <morgansegura@gmail.com>
2021-02-01 15:27:33 -0800

    Updates feed styling, updates sidebar post styling

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,10 +1,36 @@
-import React from "react";
-import { useFeedSideSuggestionsStyles } from "../../styles";
+import React from 'react'
+import { useFeedSideSuggestionsStyles } from '../../styles'
+import { Paper, Typography } from '@material-ui/core'
+import { getDefaultUser } from '../../data'
+import UserCard from '../shared/UserCard'
+import FollowButton from '../shared/FollowButton'
 
-function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
+function FeedSideSuggestions({ user }) {
+    const classes = useFeedSideSuggestionsStyles()
 
-  return <div>FeedSideSuggestions</div>;
+    return (
+        <article className={classes.article}>
+            <Paper className={classes.paper}>
+                <Typography
+                    color="textSecondary"
+                    variant="subtitle2"
+                    component="h2"
+                    gutterBottom
+                    className={classes.typography}
+                >
+                    Suggestion For You
+                </Typography>
+                {Array.from({ length: 5 }, () => getDefaultUser()).map(
+                    (user) => (
+                        <div key={user.id} className={classes.card}>
+                            <UserCard user={user} />
+                            <FollowButton side />
+                        </div>
+                    )
+                )}
+            </Paper>
+        </article>
+    )
 }
 
-export default FeedSideSuggestions;
+export default FeedSideSuggestions
